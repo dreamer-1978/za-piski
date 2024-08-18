de8bd1389ab6b7bf045418d78a33a5a285962f84
dreamer-1978 <dreamer-1978@mail.ru>
2024-08-18 21:30:53 +0300

    5

--- main.js
+++ main.js
@@ -9,33 +9,6 @@ const refs = {
 refs.formTodo.addEventListener("submit", onSubmit);
 refs.list.addEventListener("click", onClickByList);
 
-//   initTodo(list) {
-//     this.todoList = list;
-//   },
-
-//   addTodo(task) {
-//     const newTodo = {
-//       id: Date.now().toString(),
-//       task,
-//       isCompleted: false,
-//     };
-//     this.todoList = [...this.todoList, newTodo];
-//     return newTodo;
-//   },
-//   changeStatus(id) {
-//     this.todoList = this.todoList.map((todo) => {
-//       if (todo.id === id) {
-//         return { ...todo, isCompleted: !todo.isCompleted };
-//       }
-//       return todo;
-//     });
-//   },
-
-//   removeTodo(id) {
-//     this.todoList = this.todoList.filter((todo) => todo.id !== id);
-//   },
-// };
-
 const addInitList = () => {
   let listTodo = localStorage.getItem("list");
   if (!listTodo) return;
